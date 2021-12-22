537b07273974b7c637498038f0fb333bdaa4b029
S Roze <sherozem4@gmail.com>
2021-12-22 11:27:17 +0000

    changes have been made, where the overlay now shows up. A function has been created to reduce repeated code, and when the user clicks/crosses off the modal the state of the overlay changes accordingly

--- starter/script.js
+++ starter/script.js
@@ -1,15 +1,26 @@
 'use strict';
 
+document.querySelector('.show-modal').textContent = "changeMane";
+document.querySelector('button').style.backgroundColor = 'yellow';
 
 function showOrHide(state) { 
     document.querySelector('.hidden').style.display = state;
 }
 
+function overlayState(state) { 
+    document.querySelector('.overlay').style.display = state;
+}
+
+
+
+
 document.querySelector('.show-modal').addEventListener('click', function () {
   showOrHide("block");
+  overlayState("block");
 });
 
 document.querySelector('.close-modal').addEventListener('click', function () { 
     showOrHide("none");
+    overlayState("none");
 });
 
