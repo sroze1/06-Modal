700edc26fcdc43835362274ed1c1b1ee2b4a46af
S Roze <sherozem4@gmail.com>
2021-12-22 11:32:30 +0000

    2 functions have been reduced into one, as the only thing that is changing is the state for both occurences, this has been added into one function rather than having seperate ones

--- starter/script.js
+++ starter/script.js
@@ -3,25 +3,24 @@
 // Functions to change the state of the modal and the overlay
 function showOrHide(state) { 
     document.querySelector('.hidden').style.display = state;
-}
-
-function overlayState(state) { 
     document.querySelector('.overlay').style.display = state;
 }
+
+
 
 
 
 // Event listeners for the click function once the modal's have been clicked or unclicked.
 document.querySelector('.show-modal').addEventListener('click', function () {
   showOrHide("block");
-  overlayState("block");
+  
 });
 
 document.querySelector('.close-modal').addEventListener('click', function () { 
     showOrHide("none");
-    overlayState("none");
 });
 
 
 
 
+const state = document.querySelector('.show-modal').addEventListener('click')
